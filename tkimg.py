0f63e37e3a41a250e80f3232529eff27d851a053
William Tumeo <tumeowilliam@gmail.com>
2019-06-29 02:14:57 -0300

    Fix Tk import on Heroku

--- tkimg.py
+++ tkimg.py
@@ -1,18 +1,25 @@
 
-from tkinter import Tk, Label
-from PIL import ImageTk
+# Some envs don't have Python configured for Tk, such as Heroku.
+_TK_SUPPORT = True
+try:
+    from tkinter import Tk, Label
+    from PIL import ImageTk
+except ModuleNotFoundError:
+    _TK_SUPPORT = False
 
 ROOT = None
 IMG = None
 PANEL = None
 
 def init_tk():
+    if not _TK_SUPPORT: return
     global ROOT, PANEL
     ROOT = Tk()
     ROOT.title('Rinha de Galo BOT [Tk]')
     PANEL = Label(ROOT)
 
 def show_img(img):
+    if not _TK_SUPPORT: return
     global IMG, PANEL
     IMG = ImageTk.PhotoImage(img)
     PANEL.configure(image=IMG)
